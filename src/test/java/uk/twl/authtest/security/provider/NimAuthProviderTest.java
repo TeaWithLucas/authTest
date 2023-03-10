2334618fa1c6749edec41f1130322143393b30c1
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-03-10 17:50:30 +0000

    tests

--- src/test/java/uk/twl/authtest/security/provider/NimAuthProviderTest.java
+++ src/test/java/uk/twl/authtest/security/provider/NimAuthProviderTest.java
@@ -1,55 +1,39 @@
 package uk.twl.authtest.security.provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-import static org.mockito.BDDMockito.given;
 
-import java.util.List;
-import java.util.Map.Entry;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Kc Auth Provider Unit Tests")
-class KcAuthProviderTest {
+class NimAuthProviderTest {
 
   public static final String USERNAME = "test";
   public static final String PASSWORD = "test123";
-  private KcAuthProvider kcAuthProvider;
+  @InjectMocks
+  private NimAuthProvider nimAuthProvider;
 
-  @Mock
-  private BearerTokenResolver userBearerTokenResolver;
   @Mock
   private HttpServletRequest httpServletRequest;
-
-  private static String HEADER_NAME = "testHeader";
-
-  @BeforeEach
-  void setUp() {
-    kcAuthProvider = new KcAuthProvider(HEADER_NAME, userBearerTokenResolver);
-  }
 
 
   @Test
   @DisplayName("Test authenticate method with valid username and password")
   void testAuthenticateWithValidUsernameAndPassword() {
     // Given / When
-    MultiValueMap<String, String> result = kcAuthProvider.authenticate(USERNAME, PASSWORD);
+    MultiValueMap<String, String> result = nimAuthProvider.authenticate(USERNAME, PASSWORD);
 
     // Then
-    assertThat(result)
-        .hasSize(1)
-        .extractingFromEntries(Entry::getKey, Entry::getValue)
-        .containsOnly(tuple(HEADER_NAME, List.of("Bearer sdfsdfsd.sdfsdfsd.sdfsdfsd")));
+    assertThat(result).isEmpty();
   }
 
 
@@ -57,23 +41,17 @@ class KcAuthProviderTest {
   @DisplayName("Test authenticate method with null username and password")
   void testAuthenticateWithNullUsernameAndPassword() {
     // Given / When
-    MultiValueMap<String, String> result = kcAuthProvider.authenticate(null, null);
+    MultiValueMap<String, String> result = nimAuthProvider.authenticate(null, null);
 
     // Then
-    assertThat(result)
-        .hasSize(1)
-        .extractingFromEntries(Entry::getKey, Entry::getValue)
-        .containsOnly(tuple(HEADER_NAME, List.of("Bearer sdfsdfsd.sdfsdfsd.sdfsdfsd")));
+    assertThat(result).isEmpty();
   }
 
   @Test
   @DisplayName("Test authenticate method with HttpServletRequest")
   void testAuthenticateWithHttpServletRequest() {
-    // Given
-    given(userBearerTokenResolver.resolve(httpServletRequest)).willReturn("Bearer token");
-
-    // When
-    boolean result = kcAuthProvider.authenticate(httpServletRequest);
+    // Given / When
+    boolean result = nimAuthProvider.authenticate(httpServletRequest);
 
     // Then
     assertThat(result).isTrue();
@@ -83,10 +61,10 @@ class KcAuthProviderTest {
   @DisplayName("Test authenticate method with null HttpServletRequest")
   void testAuthenticateWithNullHttpServletRequest() {
     // Given / When
-    boolean result = kcAuthProvider.authenticate(null);
+    boolean result = nimAuthProvider.authenticate(null);
 
     // Then
-    assertThat(result).isFalse();
+    assertThat(result).isTrue();
   }
 
   @Test
@@ -96,9 +74,9 @@ class KcAuthProviderTest {
     MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
     // When
-    UUID result = kcAuthProvider.getUserId(headers);
+    UUID result = nimAuthProvider.getUserId(headers);
 
     // Then
-    assertThat(result).isNotNull();
+    assertThat(result).isNull();
   }
 }
