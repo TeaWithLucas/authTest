d6ed43dabcf8db261b7063540a505cabe4e417e0
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-03-13 13:40:28 +0000

    Update SecurityProperties.java

--- src/main/java/uk/twl/authtest/security/config/SecurityProperties.java
+++ src/main/java/uk/twl/authtest/security/config/SecurityProperties.java
@@ -7,13 +7,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+/**
+
+ This class holds the security properties for the application.
+ */
 @Configuration
 @EnableWebSecurity
 @ConfigurationProperties(prefix = "security")
 @Data
 public class SecurityProperties {
+  /** The list of anonymous paths */
   private final List<String> anonymousPaths = new ArrayList<>();
 
+  /**
+   Returns the array of anonymous paths.
+   
+   @return the array of anonymous paths
+   */
   public String[] getAnonymousPathsArray() {
     return anonymousPaths.toArray(String[]::new);
   }
